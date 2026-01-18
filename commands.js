cef2c71f3c04ddddfdb85d0640d26feaa981e05d
Calyx <calyxdrey11@gmail.com>
2026-01-18 19:36:08 +0200

    Add command definitions for bot functionality

---
+++ commands.js
@@ -0,0 +1,6 @@
+export const commands = {
+  menu: "📜 MENU\n.menu\n.info\n.about\n.ping",
+  info: "ℹ️ INFO\nBaileys Pair Code Bot",
+  about: "👤 ABOUT\nRender-safe WhatsApp bot",
+  ping: "🏓 Pong!"
+};
